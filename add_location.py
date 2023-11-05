a275aed1eb21d1ec8aab8513054dc8fece3d7c4e
pardodavid00 <149871848+pardodavid00@users.noreply.github.com>
2023-11-05 10:35:47 +0100

    Update add_location.py

--- add_location.py
+++ add_location.py
@@ -1,22 +1,20 @@
-#Create Users Table
+#Create Location Table
 
 try:
     #table_name variable
-    usersTable="users"
-    create_usersTablee_query = '''CREATE TABLE '''+ usersTable +''' 
-              (username TEXT  PRIMARY KEY     NOT NULL,
-               name           TEXT    NOT NULL,
-               age        INT    NOT NULL,
-               n_post          INT NOT NULL,
-               followers          INT NOT NULL
+    locationTable="location"
+    create_locationTablee_query = '''CREATE TABLE '''+ locationTable +''' 
+              (city TEXT  PRIMARY KEY     NOT NULL,
+               country           TEXT    NOT NULL,
+               interactions      INT NOT NULL
                ); '''
 
     #Execute this command (SQL Query)
-    cursor.execute(create_usersTablee_query)
+    cursor.execute(create_locationTablee_query)
     
     # Make the changes to the database persistent
     con.commit()
-    print("Table ("+ usersTable +") created successfully in PostgreSQL ")
+    print("Table ("+ locationTable +") created successfully in PostgreSQL ")
 except (Exception, psycopg2.Error) as error:
     # if it exits with an exception the transaction is rolled back.
     con.rollback()
@@ -24,7 +22,7 @@ except (Exception, psycopg2.Error) as error:
 
 cursor = con.cursor()
 
-file_dir = str(pathlib.Path().absolute()) + '/data/user.csv'
+file_dir = str(pathlib.Path().absolute()) + '/data/location.csv'
 file = open(file_dir, "r")
 first = 0
 data_to_insert = []  # Lista para almacenar los datos a insertar
@@ -36,21 +34,20 @@ for line in file:
     work_line = line.strip().split(",")  # Elimina el salto de línea y divide por comas
     data_to_insert.append(work_line)  # Agrega los datos a la lista
 
-sql_insert_users = "INSERT INTO users (username, name, age, n_post, followers) VALUES (%s, %s, %s, %s, %s)"
-
+sql_insert_location = "INSERT INTO location (city, country, interactions) VALUES (%s, %s, %s)"
 
 try:
     # Ejecuta el INSERT statement para cada conjunto de datos
     for user_data in data_to_insert:
-        data = (user_data[0], user_data[1],user_data[2],user_data[3], user_data[4])
-        cursor.execute(sql_insert_users, data)
+        data = (user_data[0], user_data[1],user_data[2])
+        cursor.execute(sql_insert_location, data)
          
 
     # Realiza la inserción en la base de datos
     con.commit()
     # El número de filas/tuplas insertadas
     count = cursor.rowcount
-    print(count, "Registros insertados exitosamente en la tabla users")
+    print(count, "Registros insertados exitosamente en la tabla location")
 
 except (Exception, psycopg2.Error) as error:
     con.rollback()
@@ -58,5 +55,5 @@ except (Exception, psycopg2.Error) as error:
 
 #use Pandas to print the result in tabular form
 # Don't RUN before you put your SQL Query
-my_table = pd.read_sql("SELECT * FROM users", con)
+my_table = pd.read_sql("SELECT * FROM location", con)
 display(my_table)
